f32c21287f9aea1d28b1099a4dd8bcd464d7d76d
zf1976 <1596863112@qq.com>
2021-10-09 22:40:44 +0800

    refactor: 引入分布式锁 + cache aside

---
+++ mayi-common/mayi-common-component/src/main/java/com/zf1976/mayi/common/component/cache/AspectLock.java
@@ -0,0 +1,11 @@
+package com.zf1976.mayi.common.component.cache;
+
+/**
+ * 使用Redisson分布式锁
+ *
+ * @author mac
+ * 2021/10/8 星期五 3:45 下午
+ */
+public interface AspectLock {
+
+}
--- mayi-common/mayi-common-component/src/main/java/com/zf1976/mayi/common/component/cache/aspect/CacheAspect.java
+++ mayi-common/mayi-common-component/src/main/java/com/zf1976/mayi/common/component/cache/aspect/CacheAspect.java
@@ -15,6 +15,9 @@ import com.zf1976.mayi.common.security.support.session.manager.SessionManagement
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.AopProxyUtils;
@@ -25,6 +28,7 @@ import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.ReflectionUtils;
 
+import java.io.IOException;
 import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -39,16 +43,32 @@ import java.util.stream.Collectors;
 @Aspect
 @Component
 @Order(0)
-public class LoadCacheAspect {
+public class CacheAspect {
 
-    private final Logger log = LoggerFactory.getLogger("[LoadCacheAspect]");
+    private static final Logger log = LoggerFactory.getLogger("[CacheAspect]");
     private final SpringElExpressionHandler handler = new SpringElExpressionHandler();
     private Map<CacheImplement, ICache<Object, Object>> cacheProviderMap;
+    private static final RedissonClient redissonClient;
 
-    public LoadCacheAspect(RedisTemplate<Object, Object> template, GuavaCacheProperties properties) {
+    static {
+        final Config config;
+        try {
+            final var resourceAsStream = CacheAspect.class.getClassLoader().getResourceAsStream("redisson.yaml");
+            config = Config.fromYAML(resourceAsStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        redissonClient = Redisson.create(config);
+    }
+
+    public CacheAspect(RedisTemplate<Object, Object> template, GuavaCacheProperties properties) {
         this.addProvider(CacheImplement.CAFFEINE, new GuavaCacheProvider<>(properties));
         this.addProvider(CacheImplement.REDIS, new RedisCacheProvider<>(properties, template));
         this.checkStatus();
+    }
+
+    public static <T> T[] clone(T[] array) {
+        return array == null ? null : array.clone();
     }
 
     @SuppressWarnings("unchecked")
@@ -89,62 +109,25 @@ public class LoadCacheAspect {
     public Object remove(ProceedingJoinPoint joinPoint, CacheEvict annotation) throws Throwable {
         // Get the proxy class
         Class<?> targetClass = this.extractTargetClass(joinPoint.getThis());
-        // Proxy method
-        Method method = this.handler.filterMethod(joinPoint);
         // Cache configuration
-        CacheConfig classAnnotation = targetClass.getAnnotation(CacheConfig.class);
-        // namespaces
-        String namespace = classAnnotation == null ? annotation.namespace() : classAnnotation.namespace();
-        // Dependent cache space
-        String[] dependOnNamespace = addAll(classAnnotation == null ? null : classAnnotation.dependsOn(), annotation.dependsOn());
-        // Cache Key
-        String key = annotation.key();
-        // 根据缓存实现清除命名空间
-        ICache<Object, Object> cacheProvider = this.cacheProviderMap.get(annotation.implement());
+        CacheConfig cacheConfig = targetClass.getAnnotation(CacheConfig.class);
 
-        // 默认策略清除所有缓存实现的命名空间
-        if (annotation.strategy()) {
-            this.cacheProviderMap.forEach((relation, cache) -> {
-                // 清除缓存空间
-                cache.invalidate(namespace);
-                // 清除依赖缓存空间
-                for (String depend : dependOnNamespace) {
-                    cache.invalidate(depend);
-                }
-            });
-        } else {
-            // 不存在key，清除缓存空间
-            if (StringUtil.isEmpty(key)) {
-                // 清除缓存空间
-                cacheProvider.invalidate(namespace);
-                // 清除依赖缓存空间
-                for (String depend : dependOnNamespace) {
-                    cacheProvider.invalidate(depend);
-                }
-            } else {
-                key = this.handler.parse(method, joinPoint.getArgs(), key, String.class, null);
-                cacheProvider.invalidate(namespace, key);
-            }
+        // clear
+        this.clearCache(joinPoint, annotation, cacheConfig);
+
+        Object proceed;
+        final var lock = redissonClient.getFairLock(annotation.key());
+        try {
+            proceed = joinPoint.proceed();
+            List<String> methodList = Arrays.stream(addAll(annotation.postInvoke(), cacheConfig != null ? cacheConfig.postInvoke() : new String[0]))
+                                            .distinct()
+                                            .collect(Collectors.toUnmodifiableList());
+            // Execute the calling method after clearing the cache
+            this.executeMethodList(joinPoint, targetClass, methodList);
+            return proceed;
+        } finally {
+            lock.unlock();
         }
-        Object proceed = joinPoint.proceed();
-
-        List<String> methodList = Arrays.stream(addAll(annotation.postInvoke(), classAnnotation != null ? classAnnotation.postInvoke() : new String[0]))
-                                        .distinct()
-                                        .collect(Collectors.toUnmodifiableList());
-        // Execute the calling method after clearing the cache
-        if (!CollectionUtils.isEmpty(methodList)) {
-            for (String methodName : annotation.postInvoke()) {
-                Method postInvokeMethod = ReflectionUtils.findMethod(targetClass, methodName);
-                if (postInvokeMethod != null) {
-                    ReflectionUtils.invokeMethod(postInvokeMethod, joinPoint.getThis());
-                }
-            }
-        }
-        return proceed;
-    }
-
-    public static <T> T[] clone(T[] array) {
-        return array == null ? null : array.clone();
     }
 
     /**
@@ -184,6 +167,43 @@ public class LoadCacheAspect {
         return AopProxyUtils.ultimateTargetClass(proxyObj);
     }
 
+    private void clearCache(ProceedingJoinPoint joinPoint, CacheEvict annotation, CacheConfig cacheConfig) {
+        // Proxy method
+        Method method = this.handler.filterMethod(joinPoint);
+        // namespaces
+        String namespace = cacheConfig == null ? annotation.namespace() : cacheConfig.namespace();
+        // Dependent cache space
+        String[] dependOnNamespace = addAll(cacheConfig == null ? null : cacheConfig.dependsOn(), annotation.dependsOn());
+        // Clear namespace based on cache implementation
+        ICache<Object, Object> cacheProvider = this.cacheProviderMap.get(annotation.implement());
+        // Cache Key
+        String key = annotation.key();
+        // 默认策略清除所有缓存实现的命名空间
+        if (annotation.strategy()) {
+            this.cacheProviderMap.forEach((relation, cache) -> {
+                // 清除缓存空间
+                cache.invalidate(namespace);
+                // 清除依赖缓存空间
+                for (String depend : dependOnNamespace) {
+                    cache.invalidate(depend);
+                }
+            });
+        } else {
+            // 不存在key，清除缓存空间
+            if (StringUtil.isEmpty(key)) {
+                // 清除缓存空间
+                cacheProvider.invalidate(namespace);
+                // 清除依赖缓存空间
+                for (String depend : dependOnNamespace) {
+                    cacheProvider.invalidate(depend);
+                }
+            } else {
+                key = this.handler.parse(method, joinPoint.getArgs(), key, String.class, null);
+                cacheProvider.invalidate(namespace, key);
+            }
+        }
+    }
+
     private void checkStatus() {
         Assert.notNull(this.handler, "expression handler Uninitialized!");
         Assert.notNull(this.cacheProviderMap, "cache provider Uninitialized!");
@@ -195,4 +215,16 @@ public class LoadCacheAspect {
         }
         this.cacheProviderMap.put(relation, cacheProvider);
     }
+
+    private void executeMethodList(ProceedingJoinPoint joinPoint, Class<?> targetClass, List<String> methodList) {
+        if (!CollectionUtils.isEmpty(methodList)) {
+            for (String methodName : methodList) {
+                Method postInvokeMethod = ReflectionUtils.findMethod(targetClass, methodName);
+                if (postInvokeMethod != null) {
+                    ReflectionUtils.invokeMethod(postInvokeMethod, joinPoint.getThis());
+                }
+            }
+        }
+    }
+
 }
