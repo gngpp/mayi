d7af42a029efa146f633e5f755d1565fb0b9e469
zf1976 <verticle@foxmail.com>
2021-11-19 00:25:27 +0800

    feat: 预增OAuth2客户端管理

--- mayi-auth/src/main/java/com/zf1976/mayi/auth/service/OAuth2ClientService.java
+++ mayi-auth/src/main/java/com/zf1976/mayi/auth/service/OAuth2ClientService.java
@@ -1,5 +1,8 @@
 package com.zf1976.mayi.auth.service;
 
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Component;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -8,6 +11,7 @@ import java.util.regex.Pattern;
  * @author mac
  * 2021/11/12 星期五 5:03 下午
  */
+@Component
 public class OAuth2ClientService {
 
     private static final Pattern ID_SECRET_PATTERN = Pattern.compile("^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{10,20}$");
@@ -17,4 +21,11 @@ public class OAuth2ClientService {
     private final int tokenRefreshMinTime = 7200;
     private final int tokenMaxTime = 2678400;
     private final int tokenRefreshMaxTime = 5356800;
+    private final RegisteredClientRepository repository;
+
+
+    public OAuth2ClientService(RegisteredClientRepository repository) {
+        this.repository = repository;
+    }
+
 }
